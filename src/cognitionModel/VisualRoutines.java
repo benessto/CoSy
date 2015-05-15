5ed212d4bd61c756884fcae2e53c50fdf59ae573
benessto <thitigal@uni-bremen.de>
2015-05-15 20:38:26 +0200

    Immer noch v oll dsapjo' v3

--- src/cognitionModel/VisualRoutines.java
+++ src/cognitionModel/VisualRoutines.java
@@ -10,14 +10,15 @@ public class VisualRoutines {
 	
 	public static HashMap<Integer, Element> FIND_GROUP(Element element){
 		GROUP.clear();
-		
-		FIND_CONNECTION (element, 'X', -1);
+		GROUP.put(element.getKoordinates(), element);
 		FIND_CONNECTION (element, 'Y', -1);
-		FIND_CONNECTION (element, 'Y', 1);
 		FIND_CONNECTION (element, 'X', 1);
+		FIND_CONNECTION (element, 'Y', 1);
+		FIND_CONNECTION (element, 'X', -1);
 		
 		for(Iterator<Map.Entry<Integer, Element>> it = GROUP.entrySet().iterator(); it.hasNext(); ) {
 	      Map.Entry<Integer, Element> entry = it.next();
+	      System.out.println(((Element) entry.getValue()).getColorAndForm() + "("+ ((Element) entry.getValue()).getX()+"|"+ ((Element) entry.getValue()).getY()+ "), Connections:" + ((Element) entry.getValue()).getConnections());
 	      if ( ((Element) entry.getValue()).getConnections() < 2 ) { //Remove elements that have less than 2 connections
 	        it.remove();
 	        //System.out.println("Removing: "+((Element) entry.getValue()).getColorAndForm() +", connections: "+((Element) entry.getValue()).getConnections());
@@ -28,18 +29,25 @@ public class VisualRoutines {
 	}
 	
 	private static void FIND_CONNECTION(Element element, char XY, int direction) {
-		GROUP.put(element.getKoordinates(), element);
+		
+		
 		if (XY == 'X') {
-			if(element.getX()+direction >=0 && element.getX() + direction<Concentration.visual.length){
+			if(element.getX()+direction >= 0 && element.getX() + direction < Concentration.visual.length){
 				String[] colorForm = Concentration.visual[element.getX()+direction][element.getY()].split(" ");
 				if (colorForm.length > 1) {
 					Element neighbor = new Element(element.getX()+direction, element.getY(), colorForm[0], colorForm[1]);
-					if(neighbor.getColorAndForm().equals(element.getColorAndForm())){
-						element.addConnections();
-						neighbor.addConnections();
+					if(neighbor.getColorAndForm().equals(element.getColorAndForm()) && !GROUP.containsKey(neighbor.getKoordinates())){
+						GROUP.put(neighbor.getKoordinates(), neighbor);
+						GROUP.get(element.getKoordinates()).addConnections();
+						GROUP.get(neighbor.getKoordinates()).addConnections();
+						System.out.println("Found connection X between"+" Element("+GROUP.get(element.getKoordinates()).getX()+"|"+GROUP.get(element.getKoordinates()).getY()+")"+" and "+GROUP.get(neighbor.getKoordinates()).getX()+"|"+GROUP.get(neighbor.getKoordinates()).getY());
 						FIND_CONNECTION(neighbor, 'Y', -1);
 						FIND_CONNECTION(neighbor, 'Y', 1);
 						FIND_CONNECTION(neighbor, 'X', direction);
+					} else if (neighbor.getColorAndForm().equals(element.getColorAndForm())) {
+						GROUP.get(element.getKoordinates()).addConnections();
+						//GROUP.get(neighbor.getKoordinates()).addConnections();
+						System.out.println("Found connection X between"+" Element("+GROUP.get(element.getKoordinates()).getX()+"|"+GROUP.get(element.getKoordinates()).getY()+")"+" and "+GROUP.get(neighbor.getKoordinates()).getX()+"|"+GROUP.get(neighbor.getKoordinates()).getY());
 					}
 				}
 			}
@@ -50,18 +58,24 @@ public class VisualRoutines {
 				String[] colorForm = Concentration.visual[element.getX()][element.getY()+direction].split(" ");
 				if (colorForm.length > 1) {
 					Element neighbor = new Element(element.getX(), element.getY()+direction, colorForm[0], colorForm[1]);
-					if(neighbor.equals(element.getColor() + " " +element.getForm())){
-						element.addConnections();
-						neighbor.addConnections();
+					if(neighbor.getColorAndForm().equals(element.getColorAndForm())  && !GROUP.containsKey(neighbor.getKoordinates())){
+						GROUP.put(neighbor.getKoordinates(), neighbor);
+						GROUP.get(element.getKoordinates()).addConnections();
+						GROUP.get(neighbor.getKoordinates()).addConnections();
+						System.out.println("Found connection Y between"+" Element("+GROUP.get(element.getKoordinates()).getX()+"|"+GROUP.get(element.getKoordinates()).getY()+")"+" and "+GROUP.get(neighbor.getKoordinates()).getX()+"|"+GROUP.get(neighbor.getKoordinates()).getY());
 						FIND_CONNECTION(neighbor, 'X', -1);
 						FIND_CONNECTION(neighbor, 'X', 1);
 						FIND_CONNECTION(neighbor, 'Y', direction);
+					} else if (neighbor.getColorAndForm().equals(element.getColorAndForm())) {
+						GROUP.get(element.getKoordinates()).addConnections();
+						//GROUP.get(neighbor.getKoordinates()).addConnections();
+						System.out.println("Found connection Y between"+" Element("+GROUP.get(element.getKoordinates()).getX()+"|"+GROUP.get(element.getKoordinates()).getY()+")"+" and "+GROUP.get(neighbor.getKoordinates()).getX()+"|"+GROUP.get(neighbor.getKoordinates()).getY());
 					}
 				}
 			}
 		}
-		
-		Concentration.printLineSeperator();
+	return;
+		//Concentration.printLineSeperator();
 	}
 
 }
