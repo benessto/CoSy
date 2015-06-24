d66f18db8c2fe1b9a3c20a3cb244a25d17d8b29c
Phill91 <pmoeller@uni-bremen.de>
2015-06-24 13:40:18 +0200

    subcon

--- src/cognitionModel/subconsciousness.java
+++ src/cognitionModel/subconsciousness.java
@@ -2,4 +2,15 @@ package cognitionModel;
 
 public class Subconsciousness {
 
+	/**
+	 * 
+	 * @param word The Colored word which color should be returned.
+	 * @return the color of the given word.
+	 */
+	public String getColor(String word){
+		String[] wordAndColor = word.split(", ");
+		return wordAndColor[1];
+
+	}
+	
 }
