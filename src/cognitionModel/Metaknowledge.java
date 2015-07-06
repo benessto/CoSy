d600fd363f9a5576f1876149d2e1d04d606ddc17
Phill91 <pmoeller@uni-bremen.de>
2015-07-06 09:56:41 +0200

    added categories

--- src/cognitionModel/Metaknowledge.java
+++ src/cognitionModel/Metaknowledge.java
@@ -5,6 +5,11 @@ import java.util.HashMap;
 public class Metaknowledge {
 	private HashMap<String, String[]> data = new HashMap<String, String[]>();
 	
+	private String[] animals = {};
+	private String[] plants = {};
+	private String[] maschines = {};
+	private String[] films = {};
+
 	public HashMap getData() {
 		return data;
 	}
